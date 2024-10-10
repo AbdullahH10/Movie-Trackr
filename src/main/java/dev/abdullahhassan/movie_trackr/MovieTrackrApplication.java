c3b87f1b22fcf36fc509cecbb1ae958374645d98
Abdullahh10 <abdullahh10@outlook.com>
2024-10-10 11:29:52 +0600

    project init

---
+++ src/main/java/dev/abdullahhassan/movie_trackr/MovieTrackrApplication.java
@@ -0,0 +1,13 @@
+package dev.abdullahhassan.movie_trackr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieTrackrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieTrackrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/abdullahhassan/movie_trackr/MovieTrackrApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.abdullahhassan.movie_trackr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieTrackrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
